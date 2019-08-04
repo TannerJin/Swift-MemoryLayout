a1da0636c913487d699710d91c62ed2a272d4dde
jintao <>
2019-08-04 20:45:28 +0800

    fix

--- SwiftPointerDemo/SwiftRuntime/Class.swift
+++ SwiftPointerDemo/SwiftRuntime/Class.swift
@@ -7,3 +7,155 @@
 //
 
 import Foundation
+
+//    class     
+//    +-----------------------------------------------------------------------+
+//    |   struct objc_object                                                  |
+//    |   +----------------------------------------------+                    |
+//    |   |   var isa: AnyClass                          |                    |
+//    |   |   var refCount: UInt64  >>>>>>>>-----------------------------------------------------+
+//    |   |-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -|                    |                  |
+//    |   |   var attribute0: String                     |                    |                  |
+//    |   |   var attribute1: [Int]                      |                    |                  |
+//    |   |   .                                          |                    |                  |
+//    |   |   .                                          |                    |                  |
+//    |   |   .                                          |                    |                  |
+//    |   +----------------------------------------------+                    |                  |
+//    +-----------------------------------------------------------------------+                  |
+                                                                                            //   |
+//    class: NSObject                                                                            |
+//    +-----------------------------------------------------------------------+                  |
+//    |   struct objc_object                                                  |                  |
+//    |   +----------------------------------------------+                    |                  |
+//    |   |   var isa: AnyClass                          |                    |                  |
+//    |   |-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -|                    |                  |
+//    |   |   var attribute0: String                     |                    |                  |
+//    |   |   var attribute1: [Int]                      |                    |                  |
+//    |   |   .                                          |                    |                  |
+//    |   |   .                                          |                    |                  |
+//    |   |   .                                          |                    |                  |
+//    |   +----------------------------------------------+                    |                  |
+//    +-----------------------------------------------------------------------+                  |
+                                                                                            //   |
+                                                                                            //   |
+                                                                                            //   |
+//                      +------------------------------------------------------------------------+
+//                      |
+//                      V
+//  refCount(8 bytes, 64 bits)
+//     /
+//    |
+//    V
+//  InlineRefCounts {
+//      atomic<InlineRefCountBits> {
+//          strong RC + unowned RC + flags
+//          OR
+//          HeapObjectSideTableEntry*
+//      }          /
+//  }             |
+//                V
+//  HeapObjectSideTableEntry {
+//      SideTableRefCounts {
+//          object pointer
+//          atomic<SideTableRefCountBits> {
+//              strong RC + unowned RC + weak RC + flags
+//          }
+//      }
+//  }
+
+//  InlineRefCountBits (64 bits)
+//  0000000000000000000000000000000000000000000000000000000000000000
+//  ||                            |||                             ||
+//  | \         30 bits          / | \          31 bits          / |
+//  |  \   StrongExtraRefCount  /  |  \     UnownedRefCount     /  |
+//  |   \______________________/   |   \_______________________/   |
+//  |                              |                               |
+//  V                              V                               V
+// UseSlow                     IsDeiniting                     IsImmortal
+// (1 bit)                       (1 bit)                        (1 bit)
+
+//  HeapObjectSideTableEntry* (64 bits)
+//  0000000000000000000000000000000000000000000000000000000000000000
+//   ||                                                            |
+//   | \                      62 bits                             /
+//   |  \              HeapObjectSideTableEntry*                 /
+//   |   \______________________________________________________/
+//   |
+//   V
+// SideTableMark
+// (1 bit)
+
+
+public func hasWeakRefCount(_ objcPointer: UnsafeMutablePointer<UInt64>) -> Bool {
+    // UseSlow && !IsImmortal
+    let nsobjcPointer = unsafeBitCast(NSObject(), to: UnsafeMutablePointer<UInt64>.self)
+    assert((objcPointer.pointee != nsobjcPointer.pointee), "NSObject's instace has not refCount")
+    
+    let bitsValue = objcPointer.advanced(by: 1).pointee
+    
+    let isImmortalMask: UInt64 = 1
+    let isImmortalValue = bitsValue & isImmortalMask
+    
+    let useSlowMask: UInt64 = 1 << 63
+    let useSlowValue = (bitsValue & useSlowMask) >> 63
+    
+    return (useSlowValue == 1) && (isImmortalValue != 1)
+}
+
+// Return unowned reference count.
+// Note that this is not equal to the number of outstanding unowned pointers.
+public func UnownedRefCount(_ objcPointer: UnsafeMutablePointer<UInt64>) -> UInt32 {
+    assertNotNSObject(objcPointer)
+    assert(!hasWeakRefCount(objcPointer), "wait to do")
+    
+    let bitsValue = objcPointer.advanced(by: 1).pointee
+    var mask: UInt64 = 1 << 31 - 1                    // 31 bits => 111...111
+    mask <<= 1                                        // 32 bits => 111...110
+    return UInt32((bitsValue & mask) >> 1) - 1        // at init refCount (strong: 0, Unowned: 1)
+}
+
+public func StrongRefCount(_ objcPointer: UnsafeMutablePointer<UInt64>) -> UInt32 {
+    assertNotNSObject(objcPointer)
+    assert(!hasWeakRefCount(objcPointer), "wait to do")
+    
+    let bitsValue = objcPointer.advanced(by: 1).pointee
+    var mask: UInt64 = 1 << 30 - 1                    // 30 bits => 111...111
+    mask <<= 33                                       // 63 bits => 111...111000...000
+    return UInt32((bitsValue & mask) >> 33) + 1       // at init refCount (strong: 0, Unowned: 1)
+}
+
+
+// nil: has not weak reference
+public func WeakRefCount(_ objcPointer: UnsafeMutablePointer<UInt64>) -> UInt32? {
+    assertNotNSObject(objcPointer)
+    
+    if !hasWeakRefCount(objcPointer) {
+        return nil
+    }
+    
+    // wait to do
+    return nil
+}
+
+
+// MARK: - Helper
+private func assertNotNSObject(_ objcPointer: UnsafeMutablePointer<UInt64>) {
+    let nsobjcPointer = unsafeBitCast(NSObject(), to: UnsafeMutablePointer<UInt64>.self)
+    assert((objcPointer.pointee != nsobjcPointer.pointee), "NSObject's instace has not refCount")
+}
+
+private func getSileTablePointer(_ objcPointer: UnsafeMutablePointer<UInt64>) -> UnsafeMutableRawPointer? {
+    assertNotNSObject(objcPointer)
+    assert(hasWeakRefCount(objcPointer))
+    
+    let bitsValue = objcPointer.advanced(by: 1).pointee
+    let mask: UInt64 = 1 << 62 - 1                  // 62 bits => 111...111
+    let slieTableValue = bitsValue & mask
+    return UnsafeMutableRawPointer(bitPattern: UInt(slieTableValue << 3))
+}
+
+private struct HeapObjectSideTableEntry {
+    var objc: UnsafeMutableRawPointer
+    var bits: UInt64
+    var weakBits: UInt32
+}
--- SwiftPointerDemo/main.swift
+++ SwiftPointerDemo/main.swift
@@ -8,17 +8,39 @@
 
 import Foundation
 
+// MARK: ---------------------------------- Class ----------------------------------
+
+class A {
+    var a: Int = 9
+}
+
+let a1 = A()
+let a2 = a1
+let a3 = a2
+print("Class =>")
+
+let objc_A_Pointer = unsafeBitCast(a1, to: UnsafeMutablePointer<UInt64>.self)
+print("\na strong references count =>:", StrongRefCount(objc_A_Pointer))
+
+unowned var unowned_a = a1
+print("a unowned references count =>:", UnownedRefCount(objc_A_Pointer))
+
+print("a has weak references =>:", hasWeakRefCount(objc_A_Pointer))
+
+weak var weak_a1 = unowned_a
+print("a has weak references =>:", hasWeakRefCount(objc_A_Pointer))
+
+
 // MARK: ---------------------------------- Bool ----------------------------------
 var bool = true
-print("Bool =>:", bool)
-bool.valuePointer.initialize(to: -10)  // bool = false
-print("Bool.value =>", bool)
+print("\nBool =>:", bool)
+print("Bool.value =>:", bool.valuePointer.pointee)
 
 
 // MARK: ---------------------------------- String ----------------------------------
 var str1 = "Tanner Jin"
 print("\nString =>:", str1)
-print("String.address =>", str1.valuePointer!)
+print("String.address =>:", str1.valuePointer!)
 
 var str2 = "124fjieji329110*(*9289fjo"
 print("\nString =>:", str2)
@@ -67,3 +89,7 @@ let set: Set<String> = ["iOS", "Apple", "Swift"]
 print("\nSet =>:", set)
 print("Set.value.count =>:", set.countPointer.pointee)
 print("Set.value.address =>:", set.valuesPointer)
+
+
+let tuple: (Int16, Int8, Int16) = (2, 3, 5)
+print("\nHello World")
